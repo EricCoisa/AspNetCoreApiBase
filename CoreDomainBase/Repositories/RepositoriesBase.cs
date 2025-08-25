6ac019f48f40f1098d176e9792dc04ade3045717
EricCoisa <ericvitor_96@hotmail.com>
2025-08-25 13:05:32 -0300

    Extrutura

---
+++ CoreDomainBase/Repositories/RepositoriesBase.cs
@@ -0,0 +1,13 @@
+﻿using CoreDomainBase.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDomainBase.Repositories
+{
+    public class RepositoriesBase : IRepositoriesBase
+    {
+    }
+}
